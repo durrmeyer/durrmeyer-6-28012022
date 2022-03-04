fd9bdb76677e9de335bd5b7f6a5e43920dc110ba
Marie <marie.durrmeyer@gmail.com>
2022-03-04 08:55:56 +0100

    limite connexion

--- backend/middleware/limite.js
+++ backend/middleware/limite.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const limite = rateLimit({
 	windowMs: 2 * 60 * 1000, //  durée de 2 min
 	max: 5, // tentatives
-	message: 'vous avez atteint les 5 connections!, veuillez recommencer dans 2 min',
+	message: 'vous avez atteint les 5 connexions!, veuillez recommencer dans 2 min',
 	headers: true
 });
 
